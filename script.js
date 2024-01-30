716ffeae432dc833f35438ef87747b575953aba7
karmaniket <aniketv943@gmail.com>
2024-01-30 08:53:13 +0530

    pr push

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSideBar() {
+    const mobileBar = document.querySelector('.mobile-sidebar');
+    mobileBar.style.display="flex"
+}
+
+function closeSideBar() {
+    const mobileBar = document.querySelector('.mobile-sidebar');
+    mobileBar.style.display="none"
+}
+
