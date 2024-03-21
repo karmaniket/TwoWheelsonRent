e636e6f256e81dd2363c1166c48984173208de9b
karmaniket <karmaniket@gmail.com>
2024-03-21 23:03:57 +0530

    UI-update

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 function showSideBar() {
     const mobileBar = document.querySelector('.mobile-sidebar');
+    // allContent.style.display = 'none';
     mobileBar.style.display="flex"
 }
 
 function closeSideBar() {
     const mobileBar = document.querySelector('.mobile-sidebar');
+    // allContent.style.display = 'block';
     mobileBar.style.display="none"
 }
+
+
+// redirect to login page if not logged in
+
+const userInfo = sessionStorage.getItem("user-info");
+const userName = localStorage.getItem("username");
+// || !userName
+// if (!userInfo) {
+//     window.location.href = "./login.html";
+// }
+
+function postSignedOut(){
+    var popup1 = document.querySelector('.loggedIn1');
+    var popup2 = document.querySelector('.loggedIn2');
+    const specialGreet = document.getElementById("specialGreet");
+    const logout = document.getElementById("logout");
+    logout.style.display="none";
+    popup1.style.display = 'flex';
+    popup2.style.display = 'flex';
+    specialGreet.style.display = 'none';
+}
+
+function signOut() {
+    // sessionStorage.removeItem('user-creds');
+    // sessionStorage.removeItem('user-info');
+    sessionStorage.clear();
+    localStorage.clear();
+    // closePopup();
+    postSignedOut();
+    console.log("session removed");
+  }
+
+  function checkIfloggedIn(){
+    window.location.href = "login.html";
+  }
 
---
+++ signup.js
@@ -0,0 +1,4 @@
+function clearForm() {
+    document.getElementById("MainForm").reset();
+    console.log("clr form");
+}
